7572e97aff2a7b35bf18e26c0e91c8366a2652db
Matt Parker <matt@lamplightdb.co.uk>
2012-11-07 22:02:58 +0000

    Adds mixupColors method to Y.Color

--- js/palette.js
+++ js/palette.js
@@ -45,7 +45,7 @@ var Col = Y.Color,
 	        s += 2;
 	    }
 	    return 1;
-	},
+	};
 
 
 	/**
@@ -73,65 +73,96 @@ var Col = Y.Color,
 	*/
  	Col.generatePalette = function (color, count, options) {
 
-    var options = options || {},
-    	color = color || "#FF0000",
-    	count = count || 10,
-    	// hue:
-        hMin = options.hMin || 0,
-        hMax = options.hMax || 360,
-        hStep = options.hStep || (hMax - hMin)/(count == 0 ? 1 : count),
-        // saturation:
-        sMin = options.sMin || 20, // 0 gives too many greys
-        sMax = options.sMax || 100,
-        sStep = options.sStep || 11,
-        // luminance
-        lMin = options.lMin || 20, // 0 gives repeated black
-        lMax = options.lMax || 80, // 100 gives repeated white
-        lStep = options.lStep || 19,
-        // startup
-        startColor = Col.toArray(Col.toHsl(color)),
-        h = startColor[0],
-        s = startColor[1],
-        l = startColor[2],
-        colors = [],
-        sVals = [],
-        sValsStep,
-        lVals = [],
-        lValsStep, 
-        j = 0;
+	    var options = options || {},
+	    	color = color || "#FF0000",
+	    	count = count || 10,
+	    	// hue:
+	        hMin = options.hMin || 0,
+	        hMax = options.hMax || 360,
+	        hStep = options.hStep || (hMax - hMin)/(count == 0 ? 1 : count),
+	        // saturation:
+	        sMin = options.sMin || 20, // 0 gives too many greys
+	        sMax = options.sMax || 100,
+	        sStep = options.sStep || 11,
+	        // luminance
+	        lMin = options.lMin || 20, // 0 gives repeated black
+	        lMax = options.lMax || 80, // 100 gives repeated white
+	        lStep = options.lStep || 19,
+	        // startup
+	        startColor = Col.toArray(Col.toHsl(color)),
+	        h = startColor[0],
+	        s = startColor[1],
+	        l = startColor[2],
+	        colors = [],
+	        sVals = [],
+	        sValsStep,
+	        lVals = [],
+	        lValsStep, 
+	        j = 0;
 
-    // steps for saturation
-    for (j = sMin; j < sMax; j = j + sStep) {
-        sVals.push(j);
-    }
-    sValsStep = findStep(sVals.length);
+	    // steps for saturation
+	    for (j = sMin; j < sMax; j = j + sStep) {
+	        sVals.push(j);
+	    }
+	    sValsStep = findStep(sVals.length);
 
-    // steps for luminance
-    for (j = lMin; j < lMax; j = j + lStep) {
-        lVals.push(j);
-    }
-    lValsStep = findStep(lVals.length);
+	    // steps for luminance
+	    for (j = lMin; j < lMax; j = j + lStep) {
+	        lVals.push(j);
+	    }
+	    lValsStep = findStep(lVals.length);
 
-    j = 0;
+	    j = 0;
 
-    // generate colors
-    while (colors.length < count) {
-        
-        colors.push(Col.fromArray([h,s,l], Col.STR_HSL));
-             
-        j++;   
-        h += hStep;
-        s = sVals[(j*sValsStep)%sVals.length];
-        l = lVals[(j*lValsStep)%lVals.length];
+	    // generate colors
+	    while (colors.length < count) {
+	        
+	        colors.push(Col.fromArray([h,s,l], Col.STR_HSL));
+	             
+	        j++;   
+	        h += hStep;
+	        s = sVals[(j*sValsStep)%sVals.length];
+	        l = lVals[(j*lValsStep)%lVals.length];
 
-        h = h % 360;
-        if (h > hMax) {
-            h = hMin + (h - hMax);
+	        h = h % 360;
+	        if (h > hMax) {
+	            h = hMin + (h - hMax);
+	        }
+
+
+	    }
+
+	    return colors;
+
+	};
+
+
+
+	/**
+	 Mixes up an array of colors (or anything else) predictably.  
+	 Takes every (p modulo l)-th item from the array, l = colors.length 
+	 and p is (if possible) not a divisor of l.
+
+	@method mixupColors
+	@static
+	@public
+	@param {Array} colors    Array of colors
+	@return {Array} 
+	*/
+	Col.mixupColors = function (colors) {
+	
+	    var step = findStep(colors.length),
+	    	i = 0,
+	    	ret = [];
+
+        for (; ret.length < colors.length; i = i + step) {
+            ret.push(colors[i%colors.length]);
         }
+        return ret;
+    };
 
 
-    }
 
-    return colors;
+
 
 };
