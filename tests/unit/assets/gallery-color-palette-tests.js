810969afcae81164e7f3e2daf9fd3f4a2610eece
Matt Parker <matt@lamplightdb.co.uk>
2012-11-08 00:05:00 +0000

    More tests

--- tests/unit/assets/gallery-color-palette-tests.js
+++ tests/unit/assets/gallery-color-palette-tests.js
@@ -11,6 +11,32 @@ YUI.add('gallery-color-palette-tests', function(Y) {
             	Equal(10, pal.length, 'Default generates 10 colors');
             	Equal("hsl(0, 100%, 50%)", pal[0], 'First is red');
 
+            },
+
+            "test creation of bigger palette from blue": function () {
+            	var pal = Y.Color.Palette.generate("#0000FF", 20);
+            	Equal(20, pal.length, 'generates 20 colors');
+            	Equal("hsl(240, 100%, 50%)", pal[0], 'First is blue');            
+            },
+
+            "test actual colors returned": function () {
+            	var pal = Y.Color.Palette.generate("#0000FF", 5);
+            	Equal(5, pal.length, 'generates 5 colors');
+            	Equal("hsl(240, 100%, 50%)", pal[0], 'First is blue');
+            	Equal("hsl(312, 53%, 77%)", pal[1], 'Second is pink');
+            	Equal("hsl(24, 86%, 58%)", pal[2], 'Third is orange');
+            	Equal("hsl(96, 31%, 39%)", pal[3], 'Fourth is  green');
+            	Equal("hsl(168, 64%, 20%)", pal[4], 'Fifth is dk green');
+            },
+
+            "test limit hue options": function () {
+            	var pal = Y.Color.Palette.generate("#0000FF", 5, {hMin:240, hMax: 360, hStep: 10});
+             	Equal(5, pal.length, 'generates 5 colors');
+            	Equal("hsl(240, 100%, 50%)", pal[0], 'First is blue');
+            	Equal("hsl(250, 53%, 77%)", pal[1], 'Second is blue');
+            	Equal("hsl(260, 86%, 58%)", pal[2], 'Third is blue');
+            	Equal("hsl(270, 31%, 39%)", pal[3], 'Fourth is blue');
+            	Equal("hsl(280, 64%, 20%)", pal[4], 'Fifth is blue');           	
             }
 
         });
